a7db980857c95aeaaa6d5762e8cb521792e75993
syedosamaa <isyedosama92@gmail.com>
2026-02-19 02:16:44 +0500

    Number Guessing Game

---
+++ app.js
@@ -0,0 +1,28 @@
+var input = document.getElementById("input");
+
+var guessBtn = document.getElementById("guessBtn");
+
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+
+guessBtn.addEventListener('click', function () {
+    var userGuess = Number(input.value);
+
+    if (userGuess === randomNumber) {
+
+        Swal.fire({
+            title: "Congratulations",
+            text: "You Guess Correct number",
+            icon: "success",
+            confirmButtonColor:"##347aeb"
+        });
+    }
+
+    else {
+        Swal.fire({
+            title: "Incorrect!",
+            text: "You Guess incorrect number",
+            icon: "error",
+            confirmButtonColor:"#347aeb"
+        });
+    }
+});
